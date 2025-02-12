ac85f49a22241ff6ff02c6cceb94bf751d5e917d
ChandraAmuda <chandra27.amuda@gmail.com>
2025-02-11 19:00:20 -0600

    Added code for Reading Test Data from JSON, Excel and CSV files

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.ui.pages.HomePage;
+import com.ui.pojo.User;
 
 public class LoginTest {
 
@@ -18,10 +19,26 @@ public class LoginTest {
 	}
 
 	@Test(description = "Verifies with the Valid User is able to login to the application", groups = { "e2e",
-			"sanity" })
-	public void loginTest() {
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestDataProvider")
+	public void loginTest(User user) {
 
-		assertEquals(homePage.goToLoginPage().doLoginWith("pesax82429@sectorid.com", "Password@1234").getUserName(),
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Cha Am");
+	}
+
+	@Test(description = "Verifies with the Valid User is able to login to the application", groups = { "e2e",
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider")
+	public void loginCSVTest(User user) {
+
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Cha Am");
+	}
+
+	@Test(description = "Verifies with the Valid User is able to login to the application", groups = { "e2e",
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider")
+	public void loginExcelTest(User user) {
+
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
 				"Cha Am");
 	}
 
